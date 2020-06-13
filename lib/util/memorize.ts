44c3b14e9a3d7b810a224e78174174683188a22d
svpcadmin <PhilisPaxil@gmail.com>
2020-06-14 01:57:22 +0900

    add: memorize

---
+++ lib/util/memorize.ts
@@ -0,0 +1,20 @@
+export const memorize: <T>(fn: () => T) => () => T = (fn) => {
+  let memorized = false;
+
+  let result:unknown;
+
+  return () => {
+    if (memorized) {
+      return result as ReturnType<typeof fn>;
+    } else {
+      result = fn();
+      memorized = true;
+
+      // メモリを解放するため
+      //@ts-ignore
+      fn = undefined;
+
+      return result as ReturnType<typeof fn>;
+    }
+  };
+};
