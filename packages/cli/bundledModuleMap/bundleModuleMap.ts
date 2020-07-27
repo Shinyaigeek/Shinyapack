2c171069077bdb44b90aa55b63b632c00de4aa75
svpcadmin <PhilisPaxil@gmail.com>
2020-07-27 22:59:17 +0900

    add: define module property based on module map

--- packages/cli/bundledModuleMap/bundleModuleMap.ts
+++ packages/cli/bundledModuleMap/bundleModuleMap.ts
@@ -5,6 +5,8 @@ import babelTraverse from "https://jspm.dev/@babel/traverse";
 import traverseType from "../../../@types/@babel/traverse.d.ts";
 import { insertExported } from "../insertExported/insertExported.ts";
 import { insertImport } from "../insertImport/insertImport.ts";
+import { insertModules } from "../insertModules/insertModules.ts";
+import { genCallModule } from "../genCallModule/genCallModule.ts";
 
 export const bundleModuleMap: (
   targetModuleMap: Map<string, CachedModuleType>,
@@ -12,7 +14,11 @@ export const bundleModuleMap: (
 ) => File = (targetModuleMap, ast) => {
   const traverse = babelTraverse.default as typeof traverseType;
   const importAddedAst = insertImport(ast);
-  const exportAddedAst = insertExported(ast);
+  const exportAddedAst = insertExported(importAddedAst);
+  let moduleAddedAst = insertModules(exportAddedAst);
+  for (let i = 1; i < targetModuleMap.size; i++) {
+    moduleAddedAst = genCallModule(ast, targetModuleMap.get(i.toString())!);
+  }
 
-  return exportAddedAst;
+  return moduleAddedAst;
 };
